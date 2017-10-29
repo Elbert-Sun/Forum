6edf3bc479c24fe77f050a609ab6d593be441334
Elbert-Sun <sunshuai522@gmail.com>
2017-10-29 22:01:00 +0000

    change the console output color

--- src/main/java/com/elbert/controller/HelloWorld.java
+++ src/main/java/com/elbert/controller/HelloWorld.java
@@ -1,16 +1,12 @@
 package com.elbert.controller;
 
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@EnableAutoConfiguration
+@RestController
 public class HelloWorld {
 
     @RequestMapping("/")
-    @ResponseBody
     String sayHello() {
-        return "Hello World!";
+        return "Hello World!!";
     }
 }
