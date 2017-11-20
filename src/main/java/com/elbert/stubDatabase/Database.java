186248ffabcbf06eaf79107facb78e858d3f4566
Elbert-Sun <sunshuai522@gmail.com>
2017-11-20 23:20:18 +0000

    Fix the bug

--- src/main/java/com/elbert/stubDatabase/Database.java
+++ src/main/java/com/elbert/stubDatabase/Database.java
@@ -6,7 +6,7 @@ import sun.jvm.hotspot.debugger.posix.elf.ELFException;
 import java.util.ArrayList;
 
 public class Database {
-    public static ArrayList<PeopleEntity> table;
+    public static ArrayList<PeopleEntity> table = new ArrayList<PeopleEntity>();
 
     static {
         PeopleEntity Elbert = new PeopleEntity("Elbert", "123");
